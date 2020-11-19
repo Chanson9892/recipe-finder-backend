6e2f8b8c00283d18d9d4782fe5573602320f7ab4
Chanson9892 <chan.hanson5@gmail.com>
2020-11-19 10:51:00 -0800

    making progress on user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,18 @@
 class UsersController < ApplicationController
-    has
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+            @token = encode_token({ user_id: @user.id })
+            render json: { user: UserSerializer.new(@user), token: @token }, status: :created
+        else
+            render json: { error: 'failed to create user' }, status: :not_acceptable
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+
 end
