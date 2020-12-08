2a19cbbaff6dfe824adcaa964bfe001125091a0a
Chanson9892 <chan.hanson5@gmail.com>
2020-12-07 23:22:03 -0800

    changed recipe number to 15, reseeded

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,7 +13,7 @@ class RecipesController < ApplicationController
 
     def show
         response = RestClient.get "https://api.spoonacular.com/recipes/complexSearch?titleMatch=#{params[:titleMatch]}
-        &instructionsRequired=true&number=5&apiKey=#{SPOONACULAR_API_KEY}&addRecipeInformation=true&fillIngredients=true&sort=popularity&includeIngredients=#{params[:includeIngredients]}"
+        &instructionsRequired=true&number=15&apiKey=#{SPOONACULAR_API_KEY}&addRecipeInformation=true&fillIngredients=true&sort=popularity&includeIngredients=#{params[:includeIngredients]}"
         result = JSON.parse(response)
         render json: result
     end
